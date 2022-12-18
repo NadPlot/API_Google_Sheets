5ffc5f9a4af32ce2dd24425baa434da8a3fc3763
Nadegda <nadin45681@gmail.com>
2022-12-19 01:16:43 +1200

    Create class Tables

--- main.py
+++ main.py
@@ -5,47 +5,53 @@ from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
 load_dotenv()
 
-# ID гугл таблицы, куда вносим данные
-SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 
 
-# Создаем Сервис-объект для работы с Google-таблицами
-CREDENTIALS_FILE = os.getenv('CREDENTIALS_FILE') # переменная с именем файла с закрытым ключом, полученый при создании Сервисного аккаунта
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')  # ID гугл таблицы, куда вносим данные (для уже существующей)
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',
-                                                                                  'https://www.googleapis.com/auth/drive'])
-httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)  # Сервис-объект для работы с Google-таблицами
+CREDENTIALS_FILE = os.getenv('CREDENTIALS_FILE') # переменная с именем файла JSON с закрытым ключом, полученый при создании Сервисного аккаунта
+
+LOG_FILE = os.getenv('LOG_FILE')  # Путь и Имя файла с логами
 
 
-# Создание нового документа Google-таблицы
-spreadsheet = service.spreadsheets().create(body = {
-    'properties': {'title': 'Logs', 'locale': 'ru_RU'},
-    'sheets': [{'properties': {'sheetType': 'GRID',
-                               'sheetId': 0,
-                               'title': 'Sheet01',
-                               'gridProperties': {'rowCount': 8, 'columnCount': 5}}}]
-}).execute()
+class Tables:
+    def __init__(self, jsonFileName):
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonFileName, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+        self.httpAuth = self.credentials.authorize(httplib2.Http())
+        self.service = apiclient.discovery.build('sheets', 'v4', http = self.httpAuth)  # Сервис-объект для работы с Google-таблицами
+        self.driveService = apiclient.discovery.build('drive', 'v3', http = self.httpAuth)  # Предоставление доступа на чтение к google таблице
+        self.tableID = None  # для получения ID таблицы
+        self.sheetID = None  # для получения URL таблицы
 
 
-# Предоставление доступа на чтение к новому документу
-driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth)
-shareRes = driveService.permissions().create(
-    fileId = spreadsheet['spreadsheetId'],
-    body = {'type': 'anyone', 'role': 'writer'},  # доступ на редактирование кому угодно
-    fields = 'id'
-).execute()
+    # Создание новой Google-таблицы и открытие доступа к ней
+    def create_table(self, title):
+        table = self.service.spreadsheets().create(body = {
+            'properties': {'title': title, 'locale': 'ru_RU'},
+            'sheets': [{'properties': {'sheetType': 'GRID','sheetId': 0}}]
+            }).execute()
+        self.tableID = table['spreadsheetId']
+        shareRes = driveService.permissions().create(
+            fileId = spreadsheet['spreadsheetId'],
+            body = {'type': 'anyone', 'role': 'writer'},  # доступ на редактирование кому угодно
+            fields = 'id'
+        ).execute()
 
 
-# Заполнение таблицы данными
-results = service.spreadsheets().values().append(spreadsheetId = '1613BR-ljmLeW82XdQaWo3cu3CuMw59jRA2GIDjISGpo', range = "Sheet1!A5:A5", valueInputOption = "USER_ENTERED", body = {"values": [["Логи кластера №1"]]}).execute()
+    # Заполнение таблицы данными из файла с логами
+    def add_data(self):
+        if self.tableID is None:
+            self.tableID = SPREADSHEET_ID
+        with open(LOG_FILE) as f:
+            for line in f:
+                data = f.readline()
+                results = self.service.spreadsheets().values().append(
+                    spreadsheetId = self.tableID, range = "Лист1!A1:A1", valueInputOption = "USER_ENTERED", body = {"values": [[data]]}).execute()
 
+    # Получение URL созданной таблицы
+    def get_table_URL(self):
+        pass
 
-results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet['spreadsheetId'], body = {
-    "valueInputOption": "USER_ENTERED",
-    "data": [
-        {"range": "Sheet01!A1:A1",
-         "values": [["Логи кластера №1"]]},
-    ]
-}).execute()
+new_table = Tables(CREDENTIALS_FILE)
+
 
